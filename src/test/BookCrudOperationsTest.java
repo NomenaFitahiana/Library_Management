da028eed9d250fef4b26ebaef57e2e39b60857bd
Nomena Fitahiana <nomenafitahiana.randrianirina@gmail.com>
2025-02-03 22:00:52 +0300

    test: fix the unit test for read_all_books_ok

--- src/test/BookCrudOperationsTest.java
+++ src/test/BookCrudOperationsTest.java
@@ -16,17 +16,17 @@ import org.junit.jupiter.api.Test;
 public class BookCrudOperationsTest {
     BookCrudOperations subject = new BookCrudOperations();
 
-    /*@Test
+    @Test
     void read_all_books_ok(){
         Book expectedBook = new Book();
-        expectedBook.setId(8);
-        expectedBook.setBookName("Crime and Punishment");
-        expectedBook.setPageNumber(430);
-        expectedBook.setReleaseDate(java.sql.Date.valueOf(LocalDate.of(1866, 1, 1)));
+        expectedBook.setId(13);
+        expectedBook.setBookName("Dracula");
+        expectedBook.setPageNumber(400);
+        expectedBook.setReleaseDate(java.sql.Date.valueOf(LocalDate.of(1897, 05, 26)));
         expectedBook.setTopic(Topic.ROMANCE);
-        expectedBook.setAuthor("A008");
+        expectedBook.setAuthor("A005");
         
-        List<Book> actual = subject.getAll(2);
+        List<Book> actual = subject.getAll(2, 4);
 
         assertTrue(actual.contains(expectedBook));
     }
@@ -34,17 +34,17 @@ public class BookCrudOperationsTest {
     @Test
     void find_one_by_id(){
         Book expectedBook = new Book();
-        expectedBook.setId(8);
-        expectedBook.setBookName("Crime and Punishment");
-        expectedBook.setPageNumber(430);
-        expectedBook.setReleaseDate(java.sql.Date.valueOf(LocalDate.of(1866, 1, 1)));
+        expectedBook.setId(13);
+        expectedBook.setBookName("Dracula");
+        expectedBook.setPageNumber(400);
+        expectedBook.setReleaseDate(java.sql.Date.valueOf(LocalDate.of(1897, 05, 26)));
         expectedBook.setTopic(Topic.ROMANCE);
-        expectedBook.setAuthor("A008");
+        expectedBook.setAuthor("A005");
 
-        Book actual = subject.findById(8);
+        Book actual = subject.findById(13);
 
         assertEquals(expectedBook, actual);
-    }*/
+    }
 
     
 }
