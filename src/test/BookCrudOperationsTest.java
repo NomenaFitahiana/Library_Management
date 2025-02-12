973c5d1d32bec0aebec913174b58c73fce1cc45f
Nomena Fitahiana <nomenafitahiana.randrianirina@gmail.com>
2025-02-12 14:52:19 +0300

    test: test for order result

--- src/test/BookCrudOperationsTest.java
+++ src/test/BookCrudOperationsTest.java
@@ -3,6 +3,7 @@ package test;
 import dao.BookCrudOperations;
 import entity.Book;
 import entity.Criteria;
+import entity.Order;
 import entity.Topic;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -47,15 +48,19 @@ public class BookCrudOperationsTest {
         assertEquals(expectedBook, actual);
     }
 
-   /* @Test
+   @Test
     void filter_by_bookname_or_releasedate_or_both(){
         
         List<Book> expectedBook = new ArrayList<>();
-
-        Book expectedBook1 = new Book(9, "Le Petit Prince", 120, Topic.ROMANCE, Date.valueOf("1943-04-06"), "A001");
+        Book expectedBook4 = new Book (10, "1984", 328, Topic.COMEDY, Date.valueOf("1949-06-08"), "A002");
         Book expectedBook2 = new Book (13, "Dracula", 400, Topic.ROMANCE, Date.valueOf("1897-05-26"), "A005");
         Book expectedBook3 = new Book (11, "Harry Potter", 500, Topic.OTHER, Date.valueOf("1997-06-26"), "A003");
-        Book expectedBook4 = new Book (10, "1984", 328, Topic.COMEDY, Date.valueOf("1949-06-08"), "A002");
+        Book expectedBook1 = new Book(9, "Le Petit Prince", 120, Topic.ROMANCE, Date.valueOf("1943-04-06"), "A001");
+        
+      
+       
+
+        Order or = new Order("bookname", "asc");
 
         Criteria criteria = new Criteria("bookname", "bookname");
         Criteria cr = new Criteria("releasedate", "1888-01-01");
@@ -64,18 +69,19 @@ public class BookCrudOperationsTest {
         table.add(criteria);
         table.add(cr);
 
-        expectedBook.add(expectedBook1);
         expectedBook.add(expectedBook4);
-        expectedBook.add(expectedBook3);
         expectedBook.add(expectedBook2);
+        expectedBook.add(expectedBook3);
+        expectedBook.add(expectedBook1);
 
-        List<Book> actual = subject.filterByCriteria(table);
+        List<Book> actual = subject.filterByCriteria(table, or);
 
         assertEquals(expectedBook, actual);
 
        
         
-    }*/
+    }
+
 
    
     
