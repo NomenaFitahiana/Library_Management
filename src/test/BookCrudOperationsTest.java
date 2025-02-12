30e8daf37282c9912326528d48729d54ea52021e
Nomena Fitahiana <nomenafitahiana.randrianirina@gmail.com>
2025-02-12 11:05:07 +0300

    test: create the test for the filterByCriteria method

--- src/test/BookCrudOperationsTest.java
+++ src/test/BookCrudOperationsTest.java
@@ -2,11 +2,15 @@ package test;
 
 import dao.BookCrudOperations;
 import entity.Book;
+import entity.Criteria;
 import entity.Topic;
 
 import static org.junit.jupiter.api.Assertions.*;
 import  org.junit.jupiter.api.Assertions;
+
+import java.sql.Date;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
@@ -44,10 +48,36 @@ public class BookCrudOperationsTest {
     }
 
     @Test
-    void filter_by_bookname_or_releasedate_between_intervals(){
+    void filter_by_bookname_or_releasedate_or_both(){
+        
+        List<Book> expectedBook = new ArrayList<>();
+
+        Book expectedBook1 = new Book(9, "Le Petit Prince", 120, Topic.ROMANCE, Date.valueOf("1943-04-06"), "A001");
+        Book expectedBook2 = new Book (13, "Dracula", 400, Topic.ROMANCE, Date.valueOf("1897-05-26"), "A005");
+        Book expectedBook3 = new Book (11, "Harry Potter", 500, Topic.OTHER, Date.valueOf("1997-06-26"), "A003");
+        Book expectedBook4 = new Book (10, "1984", 328, Topic.COMEDY, Date.valueOf("1949-06-08"), "A002");
+
+        Criteria criteria = new Criteria("bookname", "bookname");
+        Criteria cr = new Criteria("releasedate", "1888-01-01");
+
+        List<Criteria> table = new  ArrayList<>();
+        table.add(criteria);
+        table.add(cr);
+
+        expectedBook.add(expectedBook1);
+        expectedBook.add(expectedBook4);
+        expectedBook.add(expectedBook3);
+        expectedBook.add(expectedBook2);
+
+        List<Book> actual = subject.filterByCriteria(table);
+
+        assertEquals(expectedBook, actual);
+
+       
         
     }
 
+   
     
 }
 
